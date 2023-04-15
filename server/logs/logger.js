85d9e7be65c6ba5ee130aa5b44bee473a523a82f
Walter McCue <walter.mccue@gmail.com>
2023-04-15 15:10:28 -0400

    initial set-up

---
+++ server/logs/logger.js
@@ -0,0 +1,33 @@
+/**
+ * Title: logger.js
+ * Author: Walter McCue
+ * Contributors: Thomas Schultz, Jamal Damir, Carl Logan
+ * Date: 04/15/23
+ * Description: logging for the bcrs project
+*/
+
+// Require statements
+const { appendFileSync } =require('fs');
+const { join } = require('path');
+
+// Directory for logging
+const debugLog = join(__dirname, 'debug.log')
+const errorLog = join(__dirname, 'error.log')
+
+// Function to collect date/time
+const getDateTime = () => {
+  const now = new Date()
+  return now.toLocaleString('en-US')
+}
+
+// Successful operations are logged to the debug.log
+module.exports.debugLogger = (data) => {
+  const logString = `[${getDateTime()}] server\t ${data.filename} - ${data.message}\n`
+  appendFileSync(debugLog, logString)
+}
+
+// Errors are logged to the error.log
+module.exports.errorLogger = (data) => {
+  const logString = `[${getDateTime()}] server\t ${data.filename} - ${data.message}\n`
+  appendFileSync(errorLog, logString)
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { ConfirmDialogComponent } from './shared/confirm-dialog/confirm-dialog.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ServerErrorComponent } from './pages/server-error/server-error.component';
+
+// Angular Materials
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
+// Services and Modules
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AuthLayoutComponent,
-    BaseLayoutComponent
+    BaseLayoutComponent,
+    ConfirmDialogComponent,
+    AboutComponent,
+    ContactComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
