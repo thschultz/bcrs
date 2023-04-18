82022d0b09dc8570a5db5da32c1e5f6a55912f00
Walter McCue <walter.mccue@gmail.com>
2023-04-17 22:00:40 -0400

    schemas and responses

---
+++ server/schemas/selected-security.js
@@ -0,0 +1,8 @@
+/**
+ * Title: selected-security.js
+ * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
+ * Date: 04/17/23
+ * Last Modified by: Walter McCue
+ * Last Modification Date: 04/17/23
+ * Description: selected security questions schema for the bcrs project
+*/
---
+++ server/services/base-response.js
@@ -0,0 +1,8 @@
+/**
+ * Title: base-response.js
+ * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
+ * Date: 04/17/23
+ * Last Modified by: Walter McCue
+ * Last Modification Date: 04/17/23
+ * Description: base response for the bcrs project
+*/
