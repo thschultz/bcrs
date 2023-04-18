212d70c43787bb696a1d9155fb94780600a57684
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-04-18 08:39:34 -0500

    user role schema
    user role schema

--- server/schemas/user-role.js
+++ server/schemas/user-role.js
@@ -3,6 +3,15 @@
  * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
  * Date: 04/17/23
  * Last Modified by: Walter McCue
- * Last Modification Date: 04/17/23
+ * Last Modification Date: 04/18/23 by Thomas Schultz
  * Description: user roles schema for the bcrs project
 */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userRoleSchema = new Schema({
+  text: {type: String, default: 'standard'}
+})
+
+module.exports = userRoleSchema;
