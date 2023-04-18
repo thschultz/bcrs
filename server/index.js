9dd2d6cb4242115928fff5f62b1d6c9383d889fc
jdam05 <j.damir05@gmail.com>
2023-04-18 02:42:07 -0400

    Modified connection string

--- server/index.js
+++ server/index.js
@@ -6,20 +6,20 @@
  * Last Modified by: Walter McCue
  * Last Modification Date: 04/16/23
  * Description: server set up for the bcrs project
-*/
+ */
 
 /**
  * Require statements
  */
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const createError = require('http-errors');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const createError = require("http-errors");
 
-const UsersRoute = require('./routes/users-routes');
-const RolesRoute = require('./routes/roles-routes');
-const SecurityRoute = require('./routes/security-routes');
-const ServicesRoute = require('./routes/services-routes');
+const UsersRoute = require("./routes/users-routes");
+const RolesRoute = require("./routes/roles-routes");
+const SecurityRoute = require("./routes/security-routes");
+const ServicesRoute = require("./routes/services-routes");
 
 const app = express(); // Express variable.
 
@@ -27,49 +27,52 @@ const app = express(); // Express variable.
  * App configurations.
  */
 app.use(express.json());
-app.use(express.urlencoded({'extended': true}));
-app.use(express.static(path.join(__dirname, '../dist/bcrs')));
-app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../dist/bcrs")));
+app.use("/", express.static(path.join(__dirname, "../dist/bcrs")));
 
 // default server port value.
 const PORT = process.env.PORT || 3000;
 
 // TODO: This line will be replaced with your database connection string (including username/password).
-const CONN = 'mongodb+srv://bcrs_user:s3cret@bcrs.oks3e8a.mongodb.net/?retryWrites=true&w=majority';
+const CONN =
+  "mongodb+srv://bcrs_user:s3cret@bcrs.oks3e8a.mongodb.net/bcrs?retryWrites=true&w=majority";
 
 /**
  * Database connection.
  */
-mongoose.connect(CONN).then(() => {
-  console.log('Connection to the database was successful');
-}).catch(err => {
-  console.log('MongoDB Error: ' + err.message);
-});
+mongoose
+  .connect(CONN)
+  .then(() => {
+    console.log("Connection to the database was successful");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error: " + err.message);
+  });
 
 // API request/response can be made through the corresponding route
-app.use('/api/users', UsersRoute);
-app.use('/api/security', SecurityRoute);
-app.use('/api/roles', RolesRoute);
-app.use('/api/services', ServicesRoute);
+app.use("/api/users", UsersRoute);
+app.use("/api/security", SecurityRoute);
+app.use("/api/roles", RolesRoute);
+app.use("/api/services", ServicesRoute);
 
 // Error handler for 404 errors
-app.use(function(req, res, next) {
-  next(createError(404))
-})
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // Error handler for other errors
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500)
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
   res.send({
-    type: 'error',
+    type: "error",
     status: err.status,
     message: err.message,
-    stack: req.app.get('env') === 'development' ? err.stack : undefined
-
-  })
-})
+    stack: req.app.get("env") === "development" ? err.stack : undefined,
+  });
+});
 
 // Wire-up the Express server.
 app.listen(PORT, () => {
-  console.log('Application started and listening on PORT: ' + PORT);
-})
+  console.log("Application started and listening on PORT: " + PORT);
+});
