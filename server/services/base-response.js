39599dd7c3c8ab35fdaf797953a00db8149a6813
Walter McCue <walter.mccue@gmail.com>
2023-04-17 22:11:05 -0400

    services updated

--- server/services/base-response.js
+++ server/services/base-response.js
@@ -6,3 +6,23 @@
  * Last Modification Date: 04/17/23
  * Description: base response for the bcrs project
 */
+
+
+// Allows us to store server responses
+class BaseResponse {
+  constructor (httpCode, message, data) {
+    this.httpCode = httpCode;
+    this.message = message;
+    this.data = data;
+  }
+  toObject () {
+    return {
+      httpCode: this.httpCode,
+      message: this.message,
+      data: this.data,
+      timestamp: new Date().toLocaleString('en-US')
+    }
+  }
+}
+
+module.exports = BaseResponse;
