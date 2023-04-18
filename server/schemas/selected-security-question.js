8071d0e1ce8ceff23d646548c71f6515a201f885
Walter McCue <walter.mccue@gmail.com>
2023-04-17 23:26:03 -0400

    file created

--- server/schemas/selected-security-question.js
+++ server/schemas/selected-security-question.js
@@ -6,3 +6,13 @@
  * Last Modification Date: 04/17/23
  * Description: selected security questions schema for the bcrs project
  */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let selectedSecurityQuestionSchema = new Schema({
+  questionText: { type: String },
+  answerText: { type: String }
+})
+
+module.exports = selectedSecurityQuestionSchema;
