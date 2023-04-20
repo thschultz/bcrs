8b43b435db0bbec412e2a2708aea899cbdd80fbb
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-04-19 19:13:06 -0500

    interface, services
    interface, services

---
+++ src/app/shared/models/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id?: string;
+  userName?: string;
+  password?: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  address: string;
+  email: string;
+}
