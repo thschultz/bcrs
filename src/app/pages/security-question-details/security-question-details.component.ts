ad05a5a2b952b2a4f42ba85216d5aa8b1b31ee6c
jdam05 <j.damir05@gmail.com>
2023-04-19 21:54:04 -0400

    Worked on user-create component

--- src/app/pages/security-question-details/security-question-details.component.ts
+++ src/app/pages/security-question-details/security-question-details.component.ts
@@ -8,55 +8,64 @@ import { Message } from 'primeng/api';
 @Component({
   selector: 'app-security-question-details',
   templateUrl: './security-question-details.component.html',
-  styleUrls: ['./security-question-details.component.css']
+  styleUrls: ['./security-question-details.component.css'],
 })
 export class SecurityQuestionDetailsComponent implements OnInit {
-
   question: SecurityQuestion;
   questionId: string;
   errorMessages: Message[];
 
-  editForm: FormGroup this.fb.group({
-    text: [null, Validators.compose([Validators.required])]
+  editForm: FormGroup = this.fb.group({
+    text: [null, Validators.compose([Validators.required])],
   });
 
-constructor(private route: ActivatedRoute, private fb: FormBuilder, private router: Router, private securityQuestionService: SecurityQuestionService) {
-  this.question = {} as SecurityQuestion;
-  this.errorMessages = [];
-  this.questionId = this.route.snapshot.paramMap.get('questionId') ?? '';
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router,
+    private securityQuestionService: SecurityQuestionService
+  ) {
+    this.question = {} as SecurityQuestion;
+    this.errorMessages = [];
+    this.questionId = this.route.snapshot.paramMap.get('questionId') ?? '';
 
-  this.securityQuestionService.findSecurityQuestionById(this.questionId).subscribe({
-    next: (res) => {
-      this.question = res.data;
-    },
-    error: (e) => {
-      console.log(e);
-    },
-    complete: () => {
-      this.editForm.controls['text'].setValue(this.question.text);
-    }
-  })
-}
-ngOnInit(): void {
-}
-
-save(): void {
-  const updateSecurityQuestion: SecurityQuestion = {
-    text: this.editForm.controls['text'].value
+    this.securityQuestionService
+      .findSecurityQuestionById(this.questionId)
+      .subscribe({
+        next: (res) => {
+          this.question = res.data;
+        },
+        error: (e) => {
+          console.log(e);
+        },
+        complete: () => {
+          this.editForm.controls['text'].setValue(this.question.text);
+        },
+      });
   }
-      this.securityQuestionService.updateSecurityQuestion(this.questionId, updatedSecurityQuestion).subscribe({
-    next: (res) => {
-      this.router.navigate(['/security-questions']);
-    },
-    error: (e) => {
-      this.errorMessages = [
-        { severity: 'error', summary: 'Error', detail: e.message }
-      ]
-      console.log("Error occurred while saving the updated security question.")
-    }
-  })
-}
-cancel(): void {
-  this.router.navigate(['/security-questions'])
-}
+  ngOnInit(): void {}
+
+  save(): void {
+    const updateSecurityQuestion: SecurityQuestion = {
+      text: this.editForm.controls['text'].value,
+    };
+    this.securityQuestionService
+      .updateSecurityQuestion(this.questionId, updateSecurityQuestion)
+      .subscribe({
+        next: (res) => {
+          this.router.navigate(['/security-questions']);
+        },
+        error: (e) => {
+          this.errorMessages = [
+            { severity: 'error', summary: 'Error', detail: e.message },
+          ];
+          console.log(
+            'Error occurred while saving the updated security question.'
+          );
+        },
+      });
+  }
+  cancel(): void {
+    this.router.navigate(['/security-questions']);
+  }
 }
--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../shared/services/user.service';
+import { User } from '../../shared/models/user.interface';
+import { Message } from "primeng/api";
 
 @Component({
   selector: 'app-user-create',
@@ -9,7 +11,60 @@ import { UserService } from '../../shared/services/user.service';
   styleUrls: ['./user-create.component.css'],
 })
 export class UserCreateComponent implements OnInit {
-  constructor() {}
+  form: FormGroup = this.fb.group({
+    userName: [null, Validators.compose([Validators.required])],
+    password: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'),
+      ]),
+    ],
+    firstName: [null, Validators.compose([Validators.required])],
+    lastName: [null, Validators.compose([Validators.required])],
+    phoneNumber: [null, Validators.compose([Validators.required])],
+    address: [null, Validators.compose([Validators.required])],
+
+  });
+
+  user: User;
+  userId: string:
+  errorMessages: Message[] = [];
+
+  constructor(private fb: FormBuilder, private router: Router, private userService: UserService) {
+    this.user = {} as User;
+    this.userId = '';
+  }
 
   ngOnInit(): void {}
-}
+
+  createUser(): void {
+    const newUser: User = {
+      userName: this.form.controls['userName'].value,
+      password:this.form.controls['password'].value,
+      firstName:this.form.controls['firstName'].value,
+      lastName:this.form.controls['lastName'].value,
+      phoneNumber:this.form.controls['phoneNumber'].value,
+      address:this.form.controls['address'].value,
+      email:this.form.controls['email'].value,
+    };
+
+    this.userService.createUser(newUser).subscribe({
+      next: (res) => {
+        console.log(res)
+        this.router.navigate(['/users'])
+      },
+      error: (err) => {
+        this.errorMessages = [
+          {severity: 'error', summary:'Error', detail: err.message}
+        ]
+        console.log(`Node.js server error; httpCode: ${err.httpCode};message:${err.message}`)
+        console.log(err);
+      }
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/users']);
+  }
+ }
