23162bcaad2f9fa09f5814c8d397f0f20587342c
Walter McCue <walter.mccue@gmail.com>
2023-04-19 21:22:19 -0400

    user-list update

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../shared/models/user.interface';
+import { UserService } from '../../shared/services/user.service';
+import { ConfirmationService, ConfirmEventType } from 'primeng/api'
+
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  providers: [ConfirmationService]
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  constructor(private userService: UserService, private confirmationService: ConfirmationService) {
+    this.userService.findAllUsers().subscribe({
+      next: (res) => {
+        this.users = res.data;
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
