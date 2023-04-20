6f9482b99aaa9fd2be077f83164f54f5bb50d1e7
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-04-19 19:35:19 -0500

    security and user services
    security and user services

--- src/app/shared/services/user-service.service.ts
+++ src/app/shared/services/user-service.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAllUsers(): Observable<any> {
+    return this.http.get('/api/users-routes');
+  }
+
+  findUserById(userId: string): Observable<any> {
+    return this.http.get('/api/users-routes' + userId);
+  }
+
+  createUser(user: User): Observable<any> {
+    return this.http.post('/api/users-routes/', {
+      userName: user.userName,
+      password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      address: user.address,
+      email: user.email
+    })
+  }
+
+  updateUser(userId: string, user: User): Observable<any> {
+    return this.http.put('/api/users-routes/' + userId, {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      address: user.address,
+      email: user.email
+    })
+  }
+
+  deleteUser(userId: string): Observable<any> {
+    return this.http.delete('/api/users-routes' + userId);
+  }
 }
