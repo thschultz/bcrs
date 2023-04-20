ba098ba6ee2f46a9bc12ee098457d090dc890962
jdam05 <j.damir05@gmail.com>
2023-04-19 20:47:33 -0400

    Added login Function code

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class SessionServiceService {
-
-  constructor(private http: HttpClient) { }
+export class SessionService {
+  constructor(private http: HttpClient) {}
 
   login(userName: string, password: string): Observable<any> {
     return this.http.post('/api/session/login', {
       userName,
-      password
-    })
+      password,
+    });
   }
 }
