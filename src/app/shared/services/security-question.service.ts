96efde002e8b3bf62408a9abefc9cea388168210
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-04-21 13:49:35 -0500

    update security service
    update security service

--- src/app/shared/services/security-question.service.ts
+++ src/app/shared/services/security-question.service.ts
@@ -11,26 +11,26 @@ export class SecurityQuestionService {
   constructor(private http: HttpClient) { }
 
   findAllSecurityQuestions(): Observable<any> {
-    return this.http.get('/api/security-routes');
+    return this.http.get('/api/security');
   }
 
   findSecurityQuestionById(questionId: string): Observable<any> {
-    return this.http.get('/api/security-routes' + questionId);
+    return this.http.get('/api/security' + questionId);
   }
 
   createSecurityQuestion(newSecurityQuestion: SecurityQuestion): Observable<any> {
-    return this.http.post('/api/security-routes', {
+    return this.http.post('/api/security', {
       text: newSecurityQuestion.text
     })
   }
 
   updateSecurityQuestion(questionId: string, updateSecurityQuestion: SecurityQuestion): Observable<any> {
-    return this.http.put('/api/security-routes' + questionId, {
+    return this.http.put('/api/security' + questionId, {
       text: updateSecurityQuestion.text
     })
   }
 
   deleteSecurityQuestion(questionId: string): Observable<any> {
-    return this.http.delete('/api/security-routes' + questionId);
+    return this.http.delete('/api/security' + questionId);
   }
 }
