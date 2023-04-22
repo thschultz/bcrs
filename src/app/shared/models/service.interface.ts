b3ea8ccdf016169b5558aa584721af77bfb12b77
Walter McCue <walter.mccue@gmail.com>
2023-04-21 22:46:43 -0400

    services model and service

---
+++ src/app/shared/models/service.interface.ts
@@ -0,0 +1,14 @@
+/**
+ * Title: service.interface.ts
+ * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
+ * Date: 04/19/23
+ * Last Modified by: Walter McCue
+ * Last Modification Date: 04/19/23
+ * Description: service interface for the bcrs project
+ */
+
+export interface Service {
+  _id?: string;
+  serviceName: string;
+  price: number;
+}
---
+++ src/app/shared/services/service.service.ts
@@ -0,0 +1,48 @@
+/**
+ * Title: service.service.ts
+ * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
+ * Date: 04/19/23
+ * Last Modified by: Walter McCue
+ * Last Modification Date: 04/19/23
+ * Description: service api service for the bcrs project
+ */
+
+import { Injectable } from '@angular/core';
+import { Service } from '../models/service.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  constructor(private http: HttpClient) { }
+
+    findAllServices(): Observable<any> {
+      return this.http.get('/api/service');
+    }
+
+    findServiceById(serviceId: string): Observable<any> {
+      return this.http.get('/api/service/' + serviceId);
+    }
+
+    createService(newService: Service): Observable<any> {
+      return this.http.post('/api/service', {
+        serviceName: newService.serviceName,
+        price: newService.price
+      })
+    }
+
+    updateService(serviceId: string, updateService: Service): Observable<any> {
+      return this.http.put('/api/service/' + serviceId, {
+        serviceName: updateService.serviceName,
+        price: updateService.price
+      })
+    }
+
+    deleteService(serviceId: string): Observable<any> {
+      return this.http.delete('/api/service/' + serviceId);
+    }
+
+}
