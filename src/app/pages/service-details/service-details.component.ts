7a18336c5e5c7d789cae1721dc7577b78f79e9c0
Walter McCue <walter.mccue@gmail.com>
2023-04-21 23:05:32 -0400

    service components

---
+++ src/app/pages/service-details/service-details.component.ts
@@ -0,0 +1,24 @@
+/**
+ * Title: service-details.component.ts
+ * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
+ * Date: 04/19/23
+ * Last Modified by: Walter McCue
+ * Last Modification Date: 04/19/23
+ * Description: service details component for the bcrs project
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-details',
+  templateUrl: './service-details.component.html',
+  styleUrls: ['./service-details.component.css']
+})
+export class ServiceDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/service-list/service-list.component.ts
@@ -0,0 +1,24 @@
+/**
+ * Title: service-list.component.ts
+ * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
+ * Date: 04/19/23
+ * Last Modified by: Walter McCue
+ * Last Modification Date: 04/19/23
+ * Description: service list component for the bcrs project
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-list',
+  templateUrl: './service-list.component.html',
+  styleUrls: ['./service-list.component.css']
+})
+export class ServiceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
