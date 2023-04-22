7f9232f6029877c98ec6dff0679a002dc9338f63
Walter McCue <walter.mccue@gmail.com>
2023-04-21 20:09:56 -0400

    confirm-dialog update

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../shared/models/user.interface';
 import { UserService } from '../../shared/services/user.service';
 import { ConfirmationService, ConfirmEventType } from 'primeng/api'
+import { Message } from 'primeng/api/message';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog'
 
 
 @Component({
@@ -13,8 +16,9 @@ import { ConfirmationService, ConfirmEventType } from 'primeng/api'
 export class UserListComponent implements OnInit {
 
   users: User[] = [];
+  serverMessages: Message[] = [];
 
-  constructor(private userService: UserService, private confirmationService: ConfirmationService) {
+  constructor(private userService: UserService, private confirmationService: ConfirmationService, private dialog: MatDialog) {
     this.userService.findAllUsers().subscribe({
       next: (res) => {
         this.users = res.data;
@@ -29,32 +33,60 @@ export class UserListComponent implements OnInit {
   }
 
   delete(userId: string) {
-    this.confirmationService.confirm({
-      message: 'Are you sure that you want to delete this record?',
-      header: 'Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.userService.deleteUser(userId).subscribe({
-          next: (res) => {
-            console.log('User deleted successfully');
-            this.users = this.users.filter(user => user._id !== userId)
-          },
-          error: (e) => {
-            console.log(e);
-          }
-        })
-      },
-      reject: (type: any) => {
-        switch(type) {
-          case ConfirmEventType.REJECT:
-            console.log('User rejected this operation');
-            break;
-          case ConfirmEventType.CANCEL:
-            console.log('User canceled this operation');
-            break;
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        data: {
+          header: 'Delete User',
+          body: 'Are you sure you want to delete this user?'
+        },
+
+      // User has to click one of the buttons in the dialog box to get it to close
+      disableClose: true
+    })
+
+    // Subscribe event from dialog box
+    dialogRef.afterClosed().subscribe({
+      next: (result) => {
+
+        // If delete is confirmed, the task item is deleted
+        if (result === 'confirm') {
+          this.userService.deleteUser(userId).subscribe({
+            next: (res) => {
+              console.log('User deleted successfully');
+              this.users = this.users.filter(user => user._id !== userId)
+              this.serverMessages = [
+                {
+                  severity: 'success',
+                  summary: 'Success',
+                  detail: 'Task Deleted Successfully'
+                }
+              ]
+            },
+
+            // Error if failure
+            error: (err) => {
+              this.serverMessages = [
+                {
+                  severity: 'error',
+                  summary: 'Error',
+                  detail: err.message
+                }
+              ]
+            }
+          })
+
+        } else {
+
+          // If delete is canceled, inform user of cancellation
+          this.serverMessages = [
+            {
+              severity: 'info',
+              summary: 'Info',
+              detail: 'Deletion Canceled'
+            }
+          ]
         }
       }
-    });
+    })
   }
 
 }
