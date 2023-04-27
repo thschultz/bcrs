6b343b04feb1370efa2bc7ca084e1fbe6a2a4be2
jdam05 <j.damir05@gmail.com>
2023-04-26 21:00:11 -0400

    Added Interface

--- src/app/shared/models/verify-security-question.interface.ts
+++ src/app/shared/models/verify-security-question.interface.ts
@@ -1,2 +1,8 @@
-export interface VerifySecurity {
+export interface VerifySecurityQuestion {
+  question1: string;
+  question2: string;
+  question3: string;
+  answerToQuestion1: string;
+  answerToQuestion2: string;
+  answerToQuestion3: string;
 }
