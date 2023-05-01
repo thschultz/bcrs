8d76d6762b1a40b8f2f5de7e56b15dc0eea974c0
jdam05 <j.damir05@gmail.com>
2023-05-01 17:49:10 -0400

    Added code comments

--- src/app/shared/forms/verify-security-questions/verify-security-questions.component.ts
+++ src/app/shared/forms/verify-security-questions/verify-security-questions.component.ts
@@ -5,8 +5,9 @@
  * Last Modified by: Jamal Damir
  * Last Modification Date: 04/26/23
  * Description: user security question validation for the bcrs project
-*/
+ */
 
+// Import statements
 import { Component, OnInit } from '@angular/core';
 import { SelectedSecurityQuestion } from '../../models/selected-security-question.interface';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -27,6 +28,7 @@ export class VerifySecurityQuestionsComponent implements OnInit {
   username: string;
   errorMessages: Message[];
 
+  // Security questions FormGroup
   form: FormGroup = this.fb.group({
     answerToSecurityQuestion1: [
       null,
@@ -48,11 +50,16 @@ export class VerifySecurityQuestionsComponent implements OnInit {
     private userService: UserService,
     private sessionService: SessionService
   ) {
+    // Setting the value of username property to query parameter username.
     this.username = this.route.snapshot.queryParamMap.get('username') ?? '';
+    // Setting value of errorMessages property to empty array
     this.errorMessages = [];
+    // Initializing verifySecurityQuestionsModel with an empty object of type VerifySecurityQuestionModel.
     this.verifySecurityQuestionsModel = {} as VerifySecurityQuestionModel;
+    // Setting value of selectedSecurityQuestions property to empty array
     this.selectedSecurityQuestions = [];
 
+    // find security questions by username and subscribe to the observable.
     this.userService.findSelectedSecurityQuestions(this.username).subscribe({
       next: (res) => {
         this.selectedSecurityQuestions = res.data;
@@ -62,6 +69,7 @@ export class VerifySecurityQuestionsComponent implements OnInit {
         console.log(e);
       },
       complete: () => {
+        // Setting verifySecurityQuestionsModel properties to the first 3 questions on the selectedSecurityQuestions Array.
         this.verifySecurityQuestionsModel.question1 =
           this.selectedSecurityQuestions[0].questionText;
         this.verifySecurityQuestionsModel.question2 =
@@ -75,9 +83,10 @@ export class VerifySecurityQuestionsComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   VerifySecurityQuestions() {
+    // Setting values answerToQuestion(s) to corresponding form controls values
     this.verifySecurityQuestionsModel.answerToQuestion1 =
       this.form.controls['answerToSecurityQuestion1'].value;
     this.verifySecurityQuestionsModel.answerToQuestion2 =
@@ -92,11 +101,14 @@ export class VerifySecurityQuestionsComponent implements OnInit {
       .subscribe({
         next: (res) => {
           console.log(res);
+          // if the call to the API is successful
           if (res.message === 'success') {
+            // Navigate the user to the reset-password route
             this.router.navigate(['/session/reset-password'], {
               queryParams: { isAuthenticated: 'true', username: this.username },
               skipLocationChange: true,
             });
+            // If the call to the API is not successful, set the errorMessages property to array of error data
           } else {
             this.errorMessages = [
               {
@@ -108,6 +120,7 @@ export class VerifySecurityQuestionsComponent implements OnInit {
             console.log('Unable to verify security question answers');
           }
         },
+        // logs errors
         error: (e) => {
           console.log(e);
         },
