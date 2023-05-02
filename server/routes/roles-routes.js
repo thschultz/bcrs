d8e78c38d0ab0d9d6ba5a2dc991f5606fbe8bba2
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-05-01 19:50:09 -0500

    roles routes findRoles and findRolesbyId
    roles routes findRoles and findRolesbyId

--- server/routes/roles-routes.js
+++ server/routes/roles-routes.js
@@ -17,6 +17,9 @@ const createError = require('http-errors');
 const Ajv = require('ajv');
 const BaseResponse = require('../services/base-response');
 const ErrorResponse = require('../services/error-response');
+const User = require('../models/user');
+const Role = require('../models/role');
+const { async } = require('rxjs');
 
 // Logging and Validation
 const myFile = 'roles-routes.js';
@@ -25,8 +28,59 @@ const ajv = new Ajv();
 
 // findRolesUsers
 
-// findRolesById
+router.get('/', async (req, res) => {
+  try {
+    Role.find({})
+      .where('isDisabled')
+      .equals(false)
+      .exec(function(err, roles)
+  {
+    if (err)
+    {
+      console.log(err);
+      const findAllRolesMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+      res.status(500).send(findAllRolesMongodbErrorResponse.toObject());
+    }
+    else {
+      console.log(roles);
+      const findAllRolesResponse = new BaseResponse('200', 'Query successful', roles);
+      res.json(findAllRolesResponse.toObject());
+    }
+  })
+  }
+  catch (e) {
+    console.log(e);
+    const findAllRolesCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+    res.status(500).send(findAllRolesCatchErrorResponse.toObject());
+  }
+});
 
+// findRolesById
+router.get('/:roleId', async(req, res) => {
+  try {
+    Role.findOne({'_id': req.params.roleId}, function(err, Role)
+    {
+      if (err)
+      {
+        console.log(err);
+        const findRoleByIdMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+        res.status(500).send(findRoleByIdMongodbErrorResponse.toObject());
+      }
+      else
+      {
+        console.log(role);
+        const findRoleByIdResponse = new BaseResponse('200', 'Query successful', role);
+        res.json(findRoleByIdResponse.toObject());
+      }
+    })
+  }
+  catch (e)
+  {
+    console.log(e);
+    const findRoleByIdCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+    res.status(500).send(findRoleByIdCatchErrorResponse.toObject());
+  }
+})
 // createRole
 
 // updateRole
