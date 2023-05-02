a0a7622f71b9bcfb2d69b588cf968a4c8dd3e395
jdam05 <j.damir05@gmail.com>
2023-05-01 21:15:38 -0400

    Added APIs

--- server/routes/invoice-routes.js
+++ server/routes/invoice-routes.js
@@ -17,44 +17,142 @@ const Ajv = require("ajv");
 const BaseResponse = require("../services/base-response");
 const ErrorResponse = require("../services/error-response");
 
-
 // Logging and Validation
 const myFile = "security-routes.js";
 const ajv = new Ajv();
 
 // Schema for  create and update validation
 const invoiceSchema = {
-  type: 'object',
+  type: "object",
   properties: {
     lineItems: {
-      type: 'array',
+      type: "array",
       properties: {
-        title: {type: 'string'},
-        price: {type: 'number'},
+        title: { type: "string" },
+        price: { type: "number" },
       },
       additionalProperties: false,
-      required: ['title', 'price']
+      required: ["title", "price"],
     },
-    partsAmount: {type: 'number'},
-    priceAmount: {type: 'number'},
-    laborAmount: {type: 'number'},
-    lineItemTotal: {type: 'number'},
-    total: {type: 'number'}
+    partsAmount: { type: "number" },
+    priceAmount: { type: "number" },
+    laborAmount: { type: "number" },
+    lineItemTotal: { type: "number" },
+    total: { type: "number" },
   },
-  required: ['lineItems', 'partsAmount', 'priceAmount', 'laborAmount', 'lineItemTotal', 'total'],
-  additionalProperties: false
-}
-
+  required: [
+    "lineItems",
+    "partsAmount",
+    "priceAmount",
+    "laborAmount",
+    "lineItemTotal",
+    "total",
+  ],
+  additionalProperties: false,
+};
 
 /**
  * API: http://localhost:3000/api/invoices
  */
 
-
 // createInvoice
 
+router.post("/:userName", async (req, res) => {
+  try {
+    const newInvoice = {
+      userName: req.params.userName,
+      lineItems: req.body.lineItems,
+      partsAmount: req.body.partsAmount,
+      laborAmount: req.body.laborAmount,
+      lineItemTotal: req.body.lineItemTotal,
+      total: req.body.total,
+    };
+    console.log(newInvoice);
+
+    Invoice.create(newInvoice, function (err, invoice) {
+      if (err) {
+        console.log(err);
+        const createInvoiceMongodbErrorResponse = new ErrorResponse(
+          "500",
+          "Internal server error",
+          err
+        );
+        res.status(500).send(createInvoiceMongodbErrorResponse.toObject());
+      } else {
+        console.log(invoice);
+        const createInvoiceResponse = new BaseResponse(
+          "200",
+          "Query successful",
+          invoice
+        );
+        res.json(createInvoiceResponse.toObject());
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    const createInvoiceCatchErrorResponse = new ErrorResponse(
+      "500",
+      "Internal server error",
+      e.message
+    );
+    res.status(500).send(createInvoiceCatchErrorResponse.toObject());
+  }
+});
 
 //findPurchasesByService
 
+router.get("/purchases-graph", async (req, res) => {
+  try {
+    Invoice.aggregate(
+      [
+        {
+          $unwind: "$lineItems",
+        },
+        {
+          $group: {
+            _id: {
+              title: "$lineItems.title",
+              price: "$lineItems.price",
+            },
+            count: {
+              $sum: 1,
+            },
+          },
+        },
+        {
+          $sort: {
+            "_id.title": 1,
+          },
+        },
+      ],
+      function (err, purchaseGraph) {
+        if (err) {
+          console.log(err);
+          const findPurchaseByServiceGraphMongodbErrorResponse =
+            new ErrorResponse("500", "Internal server error", err);
+          res
+            .status(500)
+            .send(findPurchaseByServiceGraphMongodbErrorResponse.toObject());
+        } else {
+          console.log(purchaseGraph);
+          const findPurchaseByServiceGraphResponse = new BaseResponse(
+            "200",
+            "Query successful",
+            purchaseGraph
+          );
+          res.json(findPurchaseByServiceGraphResponse.toObject());
+        }
+      }
+    );
+  } catch (e) {
+    console.log(e);
+    const findPurchaseByServiceCatchErrorResponse = new ErrorResponse(
+      "500",
+      "Internal server error",
+      e.message
+    );
+    res.status(500).send(findPurchaseByServiceCatchErrorResponse.toObject());
+  }
+});
 
 module.exports = router;
