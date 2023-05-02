de644cdba0f2ac06921bc0f83add0d55857aab7f
Carl Logan <carlloganjr@gmail.com>
2023-05-01 19:19:40 -0600

    added createRole and updateRole

--- server/routes/roles-routes.js
+++ server/routes/roles-routes.js
@@ -82,8 +82,90 @@ router.get('/:roleId', async(req, res) => {
   }
 })
 // createRole
+router.post('/', async(req, res) => {
+  try {
+    Role.findOne({'text': req.body.text}, (err, role) => {
+      if(err) {
+        console.log(err);
+        const findRoleMongodbError = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(findRoleMongodbError.toObject());
+      }
+      else {
+        console.log(role);
+
+        if(!role) {
+          const newRole = {
+            text: req.body.text
+          }
+
+          Role.create(newRole, (err, role) => {
+            if(err) {
+              console.log(err);
+              const createRoleMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+              res.status(500).send(createRoleMongodbErrorResponse.toObject());
+            }
+            else {
+              console.log(role);
+              const createRoleResponse = new BaseResponse(200, 'Query successful', role);
+              res.json(createRoleResponse.toObject());
+            }
+          })
+        }
+        else {
+          console.log(`Role: ${req.body.text} already exists`);
+          const roleAlreadyExists = new ErrorResponse(
+            200, `Role: ${req.body.text} already exists.
+            If you do not see the role in the list then it means it was denied.`
+          );
+          res.status(200).send(roleAlreadyExists.toObject());
+        }
+      }
+    })
+  }
+  catch (e) {
+    console.log(e);
+    const createRoleCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
+    res.status(500).send(createRoleCatchErrorResponse.toObject());
+  }
+})
 
 // updateRole
+router.put('/:roleId', async(req, res) => {
+  try {
+    Role.findOne({'_id': req.params.roleId}, (err, role) => {
+      if(err) {
+        console.log(err);
+        const updateRoleMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(updateRoleMongodbErrorResponse.toObject());
+      }
+      else {
+        console.log(role);
+
+        role.set({
+          text: req.body.text
+        });
+
+        role.save((err, updateRole) => {
+          if(err) {
+            console.log(err);
+            const updatedRoleMongodbErrorResponse = new ErrorRespone(500, 'Internal server error', err);
+            res.status(500).send(updatedRoleMongodbErrorResponse.toObject());
+          }
+          else {
+            console.log(updatedRole);
+            const updatedRoleResponse = new BaseResponse(200, 'Query successful', updateRole);
+            res.json(updatedRoleResponse.toObject());
+          }
+        });
+      }
+    });
+  }
+  catch(e) {
+    console.log(e);
+    const updateRoleCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
+    res.status(500).send(updateRoleCatchErrorResponse.toObject());
+  }
+})
 
 // deleteRole
 
