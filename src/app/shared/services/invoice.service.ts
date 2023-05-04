4e39aea7c9511bcfeb8f1165971feaac498baa70
Carl Logan <carlloganjr@gmail.com>
2023-05-03 19:14:36 -0600

    invoice.service

--- src/app/shared/services/invoice.service.ts
+++ src/app/shared/services/invoice.service.ts
@@ -2,17 +2,36 @@
  * Title: invoice.service.ts
  * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
  * Date: 05/01/23
- * Last Modified by: Walter McCue
+ * Last Modified by: Carl Logan
  * Last Modification Date: 05/01/23
  * Description: invoice api service for the bcrs project
  */
 
 import { Injectable } from '@angular/core';
+import { Invoice } from '../models/invoice';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createInvoice(userName: string, invoice: Invoice): Observable<any> {
+    return this.http.post(`/api/invoices/${userName}`, {
+      userName: userName,
+      lineItems: invoice.getLineItems(),
+      partsAmount: invoice.partsAmount,
+      laborAmount: invoice.getLaborAmount(),
+      lineItemTotal: invoice.getLineItemTotal(),
+      total: invoice.getTotal()
+    })
+  }
+
+  findPurchaseByServiceGraph(): Observable<any> {
+    return this.http.get(`/api/invoices/purchases-graph`);
+  }
 }
+
