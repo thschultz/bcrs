f1d0af6e5c3c533b272444cd0bf82ad25498b144
Carl Logan <carlloganjr@gmail.com>
2023-05-03 18:58:11 -0600

    added role.guard

--- src/app/shared/role.guard.ts
+++ src/app/shared/role.guard.ts
@@ -2,23 +2,44 @@
  * Title: role-guard.ts
  * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
  * Date: 05/01/23
- * Last Modified by: Walter McCue
+ * Last Modified by: Carl Logan
  * Last Modification Date: 05/01/23
  * Description: angular guard for the bcrs project
  */
 
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { RoleService } from './services/role.service';
+import { Role } from './models/role.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  role: Role;
+
+  constructor(private router: Router, private cookieService: CookieService, private roleService: RoleService) {
+    this.role = {} as Role;
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.roleService.findUserRole(this.cookieService.get('sessionuser')).pipe(map(res => {
+      this.role = res.data;
+
+      console.log('User role: ' + this.role.text);
+      console.log(this.role);
+
+      if(res.data.text === 'admin') {
+        return true;
+      }
+      else {
+        this.router.navigate(['/']);
+        return false;
+      }
+    }))
   }
 
 }
