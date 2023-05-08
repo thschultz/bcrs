66fcf6e43e6a2493da76688d85cb6240045d4ac8
jdam05 <j.damir05@gmail.com>
2023-05-07 23:41:14 -0400

    Added code comments

--- src/app/shared/models/invoice.ts
+++ src/app/shared/models/invoice.ts
@@ -2,14 +2,17 @@
  * Title: invoice.ts
  * Authors: Thomas Schultz, Jamal Damir, Carl Logan, Walter McCue
  * Date: 05/01/23
- * Last Modified by: Walter McCue
- * Last Modification Date: 05/01/23
+ * Last Modified by: Jamal Damir
+ * Last Modification Date: 05/07/23
  * Description: Invoice model for the bcrs project
  */
 
+// Imports
 import { LineItem } from './line-item.interface';
 
+// Invoice class
 export class Invoice {
+  // Declaring private properties
   private username: string;
   private lineItems: LineItem[];
   private orderDate: string;
@@ -26,18 +29,22 @@ export class Invoice {
     this.lineItems = [];
   }
 
+  // Returns the username associated with the invoice
   getUsername(): string {
     return this.username;
   }
 
+  // Sets the lineItems property to lineItems array
   setLineItems(lineItems: LineItem[]): void {
     this.lineItems = lineItems;
   }
 
+  // Return an array of line items
   getLineItems(): LineItem[] {
     return this.lineItems;
   }
 
+  // Calculates the total value of the line items
   getLineItemTotal(): number {
     let total: number = 0;
 
@@ -48,14 +55,17 @@ export class Invoice {
     return Number(total);
   }
 
+  // Calculates order amount
   getLaborAmount(): number {
     return Number(this.laborHours) * Number(this.LABOR_RATE);
   }
 
+  // Sets order date
   getOrderDate(): string {
     return this.orderDate;
   }
 
+  // Calculates invoice total
   getTotal(): number {
     return (
       Number(this.partsAmount) +
